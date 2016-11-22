94c4812768a6b8e8e66cb490ae4188de122db90d
Mariana Templin <mariana.acuna.templin@gmail.com>
2016-11-22 18:15:11 -0500

    buttons

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -12,6 +12,8 @@ $.each(activities, function(index){
 	$('#activity-choices').append($(`<option>${activities[index].name}</option>`));
 });
 
+
+//this is adding the hotels/restaurants/activities to the schedule
 $('[data-action = "add"]').on('click', function(e){
 	const $input = $(this);
 	const $choice = $input.siblings('select');
@@ -20,20 +22,37 @@ $('[data-action = "add"]').on('click', function(e){
 	if(($choice.data().type) === "hotel"){
 		var $hotelList = $("h4:contains('My Hotel')");
 		var $itemList = $hotelList.siblings("ul").children();
-		$itemList.append("<span>" + $valueToAdd + "</span>");
+		
+		//console.log($itemList.children().length);
+		if($itemList.children().length < 2){
+			$itemList.append("<span class='title'>" + $valueToAdd + "</span>");
+			$itemList.append('<button class="btn btn-xs btn-danger remove btn-circle">x</button>');
+		}
 	} 
 
 	if(($choice.data().type) === "restaurant"){
 		var $hotelList = $("h4:contains('My Restaurants')");
 		var $itemList = $hotelList.siblings("ul").children();
-		$itemList.append("<span>" + $valueToAdd + "</span>");
+		if($itemList.children().length < 4){
+			$itemList.append("<span class='title'>" + $valueToAdd + "</span>");
+			$itemList.append('<button class="btn btn-xs btn-danger remove btn-circle">x</button>');
+		}
 	}
 
 	if(($choice.data().type) === "activity"){
 		var $hotelList = $("h4:contains('My Activities')");
 		var $itemList = $hotelList.siblings("ul").children();
-		$itemList.append("<span>" + $valueToAdd + "</span>");
+		$itemList.append("<span class='title'>" + $valueToAdd + "</span>");
+		$itemList.append('<button class="btn btn-xs btn-danger remove btn-circle">x</button>');
 	}
 })
+//below .remove is an example of event delegation
+$('.list-group').on('click', '.remove', function(e){
+	const $btn = $(this);
+	console.log(this);
+	const itemToRemove = $btn.prev('.title');
+	console.log(itemToRemove);
+})
+
 
 })
