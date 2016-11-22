70ed6aa382ce59ad9761e7dbce7517e3dcdbe27c
annaebrown <annabrown4789@gmail.com>
2016-11-22 17:24:11 -0500

    filling dem rows

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -12,4 +12,28 @@ $.each(activities, function(index){
 	$('#activity-choices').append($(`<option>${activities[index].name}</option>`));
 });
 
+$('[data-action = "add"]').on('click', function(e){
+	const $input = $(this);
+	const $choice = $input.siblings('select');
+	var $valueToAdd = $choice.val();
+
+	if(($choice.data().type) === "hotel"){
+		var $hotelList = $("h4:contains('My Hotel')");
+		var $itemList = $hotelList.siblings("ul").children();
+		$itemList.append("<span>" + $valueToAdd + "</span>");
+	} 
+
+	if(($choice.data().type) === "restaurant"){
+		var $hotelList = $("h4:contains('My Restaurants')");
+		var $itemList = $hotelList.siblings("ul").children();
+		$itemList.append("<span>" + $valueToAdd + "</span>");
+	}
+
+	if(($choice.data().type) === "activity"){
+		var $hotelList = $("h4:contains('My Activities')");
+		var $itemList = $hotelList.siblings("ul").children();
+		$itemList.append("<span>" + $valueToAdd + "</span>");
+	}
+})
+
 })
