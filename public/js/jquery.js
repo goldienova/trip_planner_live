be111007d3e66e2dcb812c5fdf64d996c741d0c4
Mariana Templin <mariana.acuna.templin@gmail.com>
2016-11-23 12:13:24 -0500

    trying to figure out switching days & having correct hotels/restaurants/activities display

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -1,26 +1,9 @@
-var day1 = {
-	hotel : "",
-	restaurant : [],
-	activites : []
-}
-var day2 = {
-	hotel : "",
-	restaurant : [],
-	activites : []
-}
-var day3 = {
-	hotel : "",
-	restaurant : [],
-	activites : []
-}
-var day4 = {
-	hotel : "",
-	restaurant : [],
-	activites : []
-}
+
+
+var days = [{},{},{},{}];
 
 $(document).ready(function(){
-
+//adding options to dropdowns from seedfile
 $.each(hotels, function(index){
 	$('#hotel-choices').append($(`<option>${hotels[index].name}</option>`));
 });
@@ -34,21 +17,32 @@ $.each(activities, function(index){
 });
 
 
+
+
+
 //this is adding the hotels/restaurants/activities to the schedule
 $('[data-action = "add"]').on('click', function(e){
 	const $input = $(this);
 	const $choice = $input.siblings('select');
 	var $valueToAdd = $choice.val();
+	//console.log($valueToAdd);
 
 	if(($choice.data().type) === "hotel"){
 		var $hotelList = $("h4:contains('My Hotel')");
 		var $itemList = $hotelList.siblings("ul").children();
+
+		//adding data to our model
+		var currentDay = parseInt($('.current-day').text());
+		days[currentDay-1].hotel = $valueToAdd;
+		//console.log(days)
 		
-		//console.log($itemList.children().length);
+		//adding data to our view
 		if($itemList.children().length < 2){
-			$itemList.append("<span class='title'>" + $valueToAdd + "</span>");
+			$itemList.append("<span class='title'>" + days[currentDay-1].hotel + "</span>");
 			$itemList.append('<button class="btn btn-xs btn-danger remove btn-circle">x</button>');
 		}
+
+		
 	} 
 
 	if(($choice.data().type) === "restaurant"){
@@ -76,11 +70,18 @@ $('.list-group').on('click', '.remove', function(e){
 	$btn.remove();
 })
 
+
+
+
+
+
+//adding days to the schedule
 $("#day-add").on('click', function(e){
 	var i = $(".day-buttons").children().length;
 	if (i <= 4) $('#day-add').before('<button class="btn btn-circle day-btn">' + i +'</button>')
 })
 
+//toggling current day below:
 $('.day-buttons').on('click', '.day-btn', function(){
 	
 	var dayObj = $(this)
@@ -90,8 +91,29 @@ $('.day-buttons').on('click', '.day-btn', function(){
 		$(this).addClass('current-day');
 		$('#day-title').children().remove()	
 		$('#day-title').append('<span>Day ' + number + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button>');
-	}
 
+
+//THIS BELOW IS WHAT WE WERE TRYING TO FIGURE OUT IN TERMS OF SHOWING CORRECT DAY INFO
+		var currentDay = parseInt($('.current-day').text());
+		//console.log(days)
+		
+
+		var $hotelList = $("h4:contains('My Hotel')");
+		var $itemList = $hotelList.siblings("ul").children();
+		//wiping view clean
+
+		$itemList.remove();
+
+
+		//adding data to our view
+
+		if($itemList.children().length < 2){
+			$itemList.append("<span class='title'>" + days[currentDay-1].hotel + "</span>");
+			$itemList.append('<button class="btn btn-xs btn-danger remove btn-circle">x</button>');
+		}
+
+
+	}
 })
 
 
